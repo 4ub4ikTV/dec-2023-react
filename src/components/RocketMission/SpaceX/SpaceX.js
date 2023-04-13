fe99daaa2052944ecccbd4bb89b9ae5e117543ec
4ub4ik <svyantkoorest@gmail.com>
2023-04-13 19:33:39 +0300

    update les2

--- src/components/RocketMission/SpaceX/SpaceX.js
+++ src/components/RocketMission/SpaceX/SpaceX.js
@@ -7,7 +7,7 @@ const SpaceX = () => {
 
     useEffect(() => {
         fetch('https://api.spacexdata.com/v3/launches/')
-            .then(response => response.json())
+            .then(value => value.json())
             .then(data => {
                 const filteredLaunches = data.filter(launch => {
                     const launchYear = new Date(launch.launch_year).getFullYear();
